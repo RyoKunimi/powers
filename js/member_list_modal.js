98fbc10019eedcda560e3305add8ad2b9f369f58
RyoheiKunimi <ryohei.kunimi@iterative.co.jp>
2020-10-21 15:20:02 +0900

    モーダル対応

--- js/member_list_modal.js
+++ js/member_list_modal.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+	var trainee = $('#trainee0');
+	console.log(trainee);
+	for (var i = 1; i <= 7; i++) {
+		trainee.clone(true).removeAttr("id").attr('id', 'trainee' + i).appendTo('.trainees');
+	}	  
+});
+	
+$( function() {
+	$('.trainees').click( function () {
+		$('#sampleModal').modal();
+	});
+});
